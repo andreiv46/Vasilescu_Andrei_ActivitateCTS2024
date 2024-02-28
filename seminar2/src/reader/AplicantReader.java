58556d9a0244addf0786fc8ab82d56344168a7b4
Ardeu <88495866+andreiv46@users.noreply.github.com>
2024-02-28 10:19:10 +0200

    seminar2

---
+++ seminar2/src/reader/AplicantReader.java
@@ -0,0 +1,27 @@
+package reader;
+
+import clase.Aplicant;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class AplicantReader {
+    public abstract List<Aplicant> read(String file) throws FileNotFoundException;
+
+    public void readAplicant(Scanner input, Aplicant aplicant){
+        String nume = input.next();
+        String prenume = (input.next()).toString();
+        int varsta = Integer.valueOf(input.nextInt());
+        int punctaj = Integer.valueOf(input.nextInt());
+        int nr = Integer.valueOf(input.nextInt());
+        String[] vect = new String[nr];
+        for (int i = 0; i < nr; i++)
+            vect[i] = input.next();
+        aplicant.setNume(nume);
+        aplicant.setPrenume(prenume);
+        aplicant.setVarsta(varsta);
+        aplicant.setPunctaj(punctaj);
+        aplicant.setNr_proiecte(nr, vect);
+    }
+}
